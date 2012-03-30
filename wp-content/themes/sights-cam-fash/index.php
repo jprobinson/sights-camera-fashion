6d9356fa376f7143f02e381d0da8883fba921f99
JP Robinson <john.paul.robinson@gmail.com>
2012-03-29 22:31:37 -0400

    adding configs and renaming template

---
+++ wp-content/themes/sights-cam-fash/index.php
@@ -0,0 +1,21 @@
+HELLOOO WORLD!!
+//<?php get_header(); ?>
+//
+//		<div id="primary">
+//			<div id="content" role="main">
+//
+//			<?php if ( have_posts() ) : ?>
+//
+//				<?php while ( have_posts() ) : the_post(); ?>
+//
+//					<?php get_template_part( 'post', get_post_format() ); ?>
+//
+//				<?php endwhile; ?>
+//
+//			<?php endif; ?>
+//
+//			</div><!-- #content -->
+//		</div><!-- #primary -->
+//
+//<?php get_sidebar(); ?>
+//<?php get_footer(); ?>
